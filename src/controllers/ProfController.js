5cc9e4988e119447f7753d1738d85e0f0766aeed
Alisson Marques <alissonlua7@gmail.com>
2020-06-16 18:39:33 -0300

    ProfController

--- src/controllers/ProfController.js
+++ src/controllers/ProfController.js
@@ -4,20 +4,27 @@ const crypto = require('crypto')
 
 module.exports = {
     async store(req, res) {
+
+        const access = 'cabral2020professores'
         
-        const { name } = req.body
+        const { name, accessCode } = req.body
 
         const id = crypto.randomBytes(4).toString('HEX')
-
+        
         try {
-            await connection('professores').insert({
-                id,
-                name, 
-            })
-            return res.json({id, name})
+            if(access === accessCode){
+                await connection('professores').insert({
+                    id,
+                    name, 
+                })
+                return res.json({id, name})
+            }else{
+                return res.json({erro: 'Código de acesso inválido!'})
+            }
         } catch (error) {
             return res.status(400).json({error: 'Erro ao cadastrar o professor!'})
         }
+
     }, 
 
 
